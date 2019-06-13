c6b6830b11a11237a78d6e01260f8028cf3adcbd
Carlo <ste.maragno@gmail.com>
2019-06-13 12:09:32 +0200

    print twice in helloworld

--- hello_world/hello_world.rs
+++ hello_world/hello_world.rs
@@ -2,4 +2,5 @@ fn main() {
     // The statements here will be executed when the compiled binary is called
     // Print text to the console
     println!("Hello World!");
+    println!("Another String");
 }
